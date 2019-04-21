b11ec95b82881513364a2c9d90e2df615ff90ad4
AlexBogdanov <alex.e.bogdanov@gmail.com>
2019-04-21 10:32:51 +0300

    Client articles component created.

--- client/src/app/components/article/articles/articles.component.ts
+++ client/src/app/components/article/articles/articles.component.ts
@@ -1,15 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
+
+import { ArticleService } from './../../../services/article.service';
+import { ArticleModel } from './../../../models/article.model';
 
 @Component({
   selector: 'app-articles',
-  templateUrl: './articles.component.html',
-  styleUrls: ['./articles.component.css']
+  templateUrl: './articles.component.html'
 })
-export class ArticlesComponent implements OnInit {
+export class ArticlesComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  private _subscribtions: Subscription[] = [];
+  public isLoading: boolean;
+  public articles: ArticleModel[];
+
+  constructor(
+    private _articleService: ArticleService,
+    private _router: Router
+  ) { }
 
   ngOnInit() {
+    this.isLoading = true;
+
+    this._subscribtions.push(
+      this._articleService.getAllArticles().subscribe(res => {
+        let j = 0;
+        const temp = [];
+        temp[j] = [];
+
+        res.data.map(article => {
+          article.shortDescr = article.description.substring(0, 30);
+          return article;
+        }).forEach(article => {
+          if (temp[j].length < 3) {
+            temp[j].push(article);
+          } else {
+            j += 1;
+            temp[j] = [];
+            temp[j].push(article);
+          }
+        });
+
+        if (temp[temp.length - 1].length < 1) {
+          temp.pop();
+        }
+
+        this.articles = temp;
+        this.isLoading = false;
+      }, err => {
+        this._router.navigate(['/']);
+        throw err;
+      })
+    );
+  }
+
+  ngOnDestroy() {
+    this._subscribtions.forEach(sub => sub.unsubscribe());
   }
 
 }
