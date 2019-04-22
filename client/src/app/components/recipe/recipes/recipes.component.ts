e6771ab873d20d8e8af15decc69e90d271a27995
AlexBogdanov <alex.e.bogdanov@gmail.com>
2019-04-22 20:30:43 +0300

    Client RecipesComponent created.

--- client/src/app/components/recipe/recipes/recipes.component.ts
+++ client/src/app/components/recipe/recipes/recipes.component.ts
@@ -1,15 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
+
+import { RecipeService } from './../../../services/recipe.service';
+import { RecipeModel } from './../../../models/recipe.model';
 
 @Component({
   selector: 'app-recipes',
-  templateUrl: './recipes.component.html',
-  styleUrls: ['./recipes.component.css']
+  templateUrl: './recipes.component.html'
 })
-export class RecipesComponent implements OnInit {
+export class RecipesComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  private _subscribtions: Subscription[] = [];
+  public isLoading: boolean;
+  public recipes: RecipeModel[];
+
+  constructor(
+    private _recipeService: RecipeService,
+    private _router: Router
+  ) { }
 
   ngOnInit() {
+    this.isLoading = true;
+
+    this._subscribtions.push(
+      this._recipeService.getAllRecipes().subscribe(res => {
+        let j = 0;
+        const temp = [];
+        temp[j] = [];
+
+        this.recipes = res.data.map(recipe => {
+          recipe.shortDescr = recipe.preparation.substring(0, 200);
+          return recipe;
+        }).forEach(recipe => {
+          if (temp[j].length < 3) {
+            temp[j].push(recipe);
+          } else {
+            j += 1;
+            temp[j] = [];
+            temp[j].push(recipe);
+          }
+        });
+
+        if (temp[temp.length - 1].length < 1) {
+          temp.pop();
+        }
+
+        this.recipes = temp;
+        this.isLoading = false;
+      }, err => {
+        this._router.navigate(['/']);
+        throw err;
+      })
+    );
+  }
+
+  ngOnDestroy() {
+    this._subscribtions.forEach(sub => sub.unsubscribe());
   }
 
 }
